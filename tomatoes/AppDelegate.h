abcd26dae780c41e8e0dc48f9a0918e6da47793b
Jenny Kwan <jennylkwan@gmail.com>
2014-01-20 03:24:00 -0800

    Initial Commit

---
+++ tomatoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tomatoes
+//
+//  Created by Jenny Kwan on 1/20/14.
+//  Copyright (c) 2014 Jenny Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tomatoes/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  tomatoes
+//
+//  Created by Jenny Kwan on 1/20/14.
+//  Copyright (c) 2014 Jenny Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
